97b1f169495f0fb7081f1777954dbb8ffadd0a4c
anjali29singh <anjalisingh930260@gmail.com>
2023-04-02 22:56:27 +0530

    modified-schema

--- test.js
+++ test.js
@@ -1,21 +1,37 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const connect =()=>{
-    return mongoose.connect('mongodb://localhost:27017/testing')
-}
+const connect = () => {
+  return mongoose.connect("mongodb://localhost:27017/testing");
+};
 const student = new mongoose.Schema({
-    firstName:String
+  firstName: {
+    type: String,
+    required: true,
+  },
+  favFoods: [{ type: String }],
+  info: {
+    school: {
+      type: String,
+    },
+    rollNo: {
+      type: Number,
+    },
+  },
+});
 
-})
-
-const Student = mongoose.model('student',student)
-
-
+const Student = mongoose.model("student", student);
 
 connect()
-.then (async connection => {
-    const student1 = await Student.create({firstName:"anjali"})
-    console.log(student1)
-})
+  .then(async (connection) => {
+    const student1 = await Student.create({
+      firstName: "anjali",
+      favFoods: ["pizza", "burger", "paste", "paneer-tikka"],
+      info:{
+        school:"Campion",
+        rollNO:41
+      }
+    });
+    console.log(student1);
+  })
 
-.catch(e => console.error(e))
+  .catch((e) => console.error(e));
